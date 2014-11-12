148e87134cea37c571053c0ef7bb19a1db3d74c9
T-baby <MYts.designer@gmail.com>
2014-11-12 23:02:41 +0800

    20141112
    小修改和格式化代码

--- ICECSS-Menu2/ice-menu.js
+++ ICECSS-Menu2/ice-menu.js
@@ -1,8 +1,8 @@
-jQuery(document).ready(function($){
-		
-//menu
+jQuery(document).ready(function($) {
 
-	$('.ice-main-nav').on('click', function(event){
-		if($(event.target).is('.ice-main-nav')) $(this).children('ul').toggleClass('is-visible');
-	});
+    //menu
+
+    $('.ice-main-nav').on('click', function(event) {
+        if ($(event.target).is('.ice-main-nav')) $(this).children('ul').toggleClass('is-visible');
+    });
 });
