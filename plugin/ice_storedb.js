0d76f44d2c4997733e90b58d0c4e90490ab76d3f
T-baby <t-baby@besdlab.cn>
2015-03-03 13:52:50 +0800

    20150303
    ****0.9.2 2015年03月03日****
    
    ******-修改/修复-******
    调整按钮内边距，使按钮看起来更优�
    修改了按钮悬浮时的颜色
    调整th、td的内边距
    修改了表格ice-table-striped的颜色
    修正了文档上的一处错误
    
    ******-增加-******
    增加ice-button标签
    增加一个用于操作本地储存库的插件ice_storedb插件

---
+++ plugin/ice_storedb.js
@@ -0,0 +1,100 @@
+var ice_storedb = function(collectionName){
+    collectionName = collectionName ? collectionName : 'default';
+    
+    var err;
+    var cache = localStorage[collectionName] ? JSON.parse(localStorage[collectionName]) : [];
+
+    return {
+
+        insert: function(obj,callback){
+            obj["_id"] = new Date().valueOf();          
+            cache.push(obj);
+            localStorage.setItem(collectionName,JSON.stringify(cache));
+            if(callback)
+                callback(err,obj);
+        },
+
+        find: function(obj, callback){
+            if(arguments.length == 0){
+                return cache;
+            } else {
+                var result = [];
+
+                for(var key in obj){
+                    for(var i = 0; i < cache.length; i++){
+                        if(cache[i][key] == obj[key]){
+                            result.push(cache[i]);
+                        }
+                    }
+                }
+                if(callback)
+                    callback(err,result);
+                else
+                    return result;
+            }     
+        },
+
+        update: function(obj,upsert,callback){
+
+            for(var key in obj){
+                for(var i = 0; i < cache.length; i++){
+                    if(cache[i][key] == obj[key]){
+
+                        end_loops:
+                        for(var upsrt in upsert){
+                            switch(upsrt){
+                            case "$inc":
+                                for(var newkey in upsert[upsrt]){
+                                    cache[i][newkey] = parseInt(cache[i][newkey]) + parseInt(upsert[upsrt][newkey]);
+                                }
+                                break;
+
+                            case "$set":
+                                for(var newkey in upsert[upsrt]){
+                                    cache[i][newkey] = upsert[upsrt][newkey];
+                                }
+                                break;
+
+                            case "$push":
+                                for(var newkey in upsert[upsrt]){
+                                    cache[i][newkey].push(upsert[upsrt][newkey]);
+                                }
+                                break;
+
+                            default:
+                                upsert['_id'] = cache[i]['_id'];
+                                cache[i] = upsert;
+                                break end_loops;
+                            }
+                        }
+                    }
+                }
+            }
+            localStorage.setItem(collectionName,JSON.stringify(cache));
+            if(callback)
+                callback(err);
+
+        },
+
+        remove: function(obj,callback){
+            if(arguments.length == 0){
+                localStorage.removeItem(collectionName);
+            } else {
+
+                for(var key in obj){
+                    for (var i = cache.length - 1; i >= 0; i--) {
+                        if(cache[i][key] == obj[key]){
+                            cache.splice(i,1);
+                        }
+                    }
+                }
+                localStorage.setItem(collectionName, JSON.stringify(cache));
+            }
+            
+            if(callback)
+                callback(err);
+            
+        }
+
+    };
+};
